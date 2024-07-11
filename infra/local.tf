a52caeba1bca2d2263576c3fc497df6992bb6b1a
Marlon Silva <josemarlondasilva48@gmail.com>
2024-07-10 23:09:00 -0300

    fix: ajuste dependencias

--- infra/local.tf
+++ infra/local.tf
@@ -1,6 +1,6 @@
 locals {
   environment_variables = {
-    USER_POOL_ID        = "us-east-1_dtnMptl6r"
+    USER_POOL_ID        = "us-east-1_F5BiZuEcH"
     DYNAMODB_TABLE_NAME = var.table_name_payment_dynamodb
     REGION_AWS          = var.region_provider_aws
     SECRET_NAME         = var.secret_name_credentials_db
